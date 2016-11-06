efda1fdac31b0207b815c8f7b0b1f433a2ccd1df
DESKTOP-BK1BEKT\tki <tki0074@gmail.com>
2016-11-06 18:32:02 +0200

    Added getters and setters for new fields

--- app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
@@ -20,6 +20,45 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public User(String firstName, String lastName, String sex, Date birth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.birth = birth;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
     public String getNick() {
         return nick;
     }
