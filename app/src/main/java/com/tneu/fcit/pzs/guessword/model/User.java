95b57ffd20bdf99d7243692bbf849ac480fade72
ViktorName <vipper125125@gmail.com>
2016-11-13 03:46:04 +0200

    Added fields : name ,secondName , point , birthDay and getters & setters for them

--- app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
@@ -1,6 +1,7 @@
 package com.tneu.fcit.pzs.guessword.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by yp on 02.11.16.
@@ -9,10 +10,19 @@ public class User implements Serializable {
     String nick;
     String password;
     int score;
+    String name;
+    String secondName;
+    String point;
+    Date birthDay;
 
-    public User(String nick, String password) {
+
+    public User(String nick, String password, String name,String secondName, String point,Date birthDay ) {
         this.nick = nick;
         this.password = password;
+        this.name = name;
+        this.secondName = secondName;
+        this.point = point;
+        this.birthDay = birthDay;
     }
 
     public String getNick() {
@@ -29,5 +39,38 @@ public class User implements Serializable {
 
     public void addScore(int value) {
         score += value;
+    }
+    //setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSecondName(String secondName)
+    {
+        this.secondName =secondName;
+    }
+    public void setPoint(String point)
+    {
+        this.point = point;
+    }
+    public  void  setBirthDay(String birthDay)
+    {
+        this.birthDay = birthDay;
+    }
+    // getters
+    public void getName() {
+        return name;
+    }
+    public void getSecondName()
+    {
+        return secondName;
+    }
+    public void getPoint()
+    {
+        return point;
+    }
+    public  void  getBirthDay()
+    {
+        return birthDay;
     }
 }
