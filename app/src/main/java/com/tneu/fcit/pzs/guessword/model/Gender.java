50e83bc0d85a7e8841c96553e746f97887a679b8
Vlad Golubev <golubev.vld@gmail.com>
2016-11-13 18:40:47 +0200

    Add parse method to gender enum

--- app/src/main/java/com/tneu/fcit/pzs/guessword/model/Gender.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/Gender.java
@@ -1,5 +1,17 @@
 package com.tneu.fcit.pzs.guessword.model;
 
 public enum Gender {
-    MALE, FEMALE, OTHER
+    MALE, FEMALE, OTHER;
+
+    public static Gender parse(String input) {
+        if (input.equalsIgnoreCase("male")) {
+            return MALE;
+        }
+
+        if (input.equalsIgnoreCase("female")) {
+            return FEMALE;
+        }
+
+        return OTHER;
+    }
 }
