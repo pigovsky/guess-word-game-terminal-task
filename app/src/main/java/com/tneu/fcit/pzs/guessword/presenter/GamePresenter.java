fe8b62c0f35df036fb1e84017ec78cbd099e2f33
Fenovka <fenovka1996@gmail.com>
2016-11-24 19:05:00 +0200

    implement checkWord and checkLetter, all unit test passed

--- app/src/main/java/com/tneu/fcit/pzs/guessword/presenter/GamePresenter.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/presenter/GamePresenter.java
@@ -47,6 +47,13 @@ public class GamePresenter {
         // TODO: Добавте код, який порівнює введене користувачем слово guess з secretWord (без врахування регістру)
         // слід змінювати рахунок користувача методом addScore (див. нижче) та викликати метод
         // gameView.showCongratulations чи gameView.showGameOver
+        if (guess.toLowerCase().equals(secretWord.toLowerCase())) {
+            addScore(100);
+            gameView.showCongratulations(secretWord);
+        } else {
+            addScore(-100);
+            gameView.showGameOver(secretWord);
+        }
     }
 
     /**
@@ -63,6 +70,26 @@ public class GamePresenter {
         // TODO: Добавте код, який перевіряє чи присутня літера letter у secretWord, модифікує getUserCurrentGuess
         // слід також змінювати рахунок користувача методом addScore та викликати метод gameView.letterHasBeenFound чи
         // gameView.letterAbsent
+        if (secretWord.toLowerCase().contains(letter.toLowerCase())) {
+
+            StringBuilder updateGuess = new StringBuilder(userCurrentGuess);
+
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (secretWord.toLowerCase().charAt(i) == letter.toLowerCase().charAt(0)) {
+                    updateGuess.setCharAt(i, secretWord.charAt(i));
+                }
+            }
+
+            userCurrentGuess = updateGuess.toString();
+
+            addScore(1);
+            gameView.letterHasBeenFound(letter.toLowerCase());
+        } else {
+            addScore(-1);
+            gameView.letterAbsent(letter.toLowerCase());
+        }
+
+
     }
 
     private void addScore(int value) {
