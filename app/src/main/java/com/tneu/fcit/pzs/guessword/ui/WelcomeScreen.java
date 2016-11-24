8913b05139ed9ae960342d878060a545f11fde82
Fenovka <fenovka1996@gmail.com>
2016-11-24 21:37:47 +0200

    view record method showBest add

--- app/src/main/java/com/tneu/fcit/pzs/guessword/ui/WelcomeScreen.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/ui/WelcomeScreen.java
@@ -6,6 +6,11 @@ import com.tneu.fcit.pzs.guessword.service.UserServiceImpl;
 import com.tneu.fcit.pzs.guessword.utils.Utils;
 import com.tneu.fcit.pzs.guessword.view.GameViewImpl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by yp on 02.11.16.
  */
@@ -14,13 +19,33 @@ public class WelcomeScreen {
     private final UserService userService = new UserServiceImpl();
 
     public void showWelcome() {
-        System.out.println("Welcome! Please [l]ogin or [r]egister");
+        System.out.println("Welcome! Please [l]ogin or [r]egister  or [s]how best results");
         String line = Utils.SCANNER.nextLine();
-        if (line.equalsIgnoreCase("l")) {
+        if ( line.equalsIgnoreCase("l") ) {
             onLogin();
-        } else if (line.equalsIgnoreCase("r")) {
+        } else if ( line.equalsIgnoreCase("r") ) {
             onRegister();
+        } else if ( line.equalsIgnoreCase("s") ) {
+            showBest();
         }
+    }
+
+    private void showBest() {
+
+        ArrayList<User> sortedUser = new ArrayList(userService.all().entrySet());
+
+        Collections.sort(sortedUser, (user1, user2) -> {
+                int scoreFirst = user1.getScore();
+                int scoreSecond = user2.getScore();
+
+                return scoreFirst < scoreSecond ? 1 : (scoreFirst > scoreSecond ? -1 : 0);
+               }
+        );
+
+        System.out.println("Records");
+        for (User user : sortedUser)
+            System.out.println(String.format("%s\t%s", user.getNick(),user.getScore()));
+
     }
 
     private void onRegister() {
@@ -28,7 +53,7 @@ public class WelcomeScreen {
         String nick;
         while (true) {
             nick = promptForNick();
-            if (userService.all().get(nick) != null) {
+            if ( userService.all().get(nick) != null ) {
                 System.err.printf("The nick %s is busy (", nick);
             } else {
                 break;
@@ -41,7 +66,7 @@ public class WelcomeScreen {
     }
 
     private static void startGameForUser(User user) {
-        System.out.println(String.format("Hello, %s !",user.getNick()));
+        System.out.println(String.format("Hello, %s !", user.getNick()));
         new GameViewImpl(user).gameLoop();
     }
 
@@ -52,7 +77,7 @@ public class WelcomeScreen {
             String nick = promptForNick();
             String pass = promptForPass();
             user = userService.check(nick, pass);
-            if (user == null) {
+            if ( user == null ) {
                 System.err.println("Wrong nick or password");
             } else {
                 break;
