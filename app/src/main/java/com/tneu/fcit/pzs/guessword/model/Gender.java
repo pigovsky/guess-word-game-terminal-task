4258eab5cfbb6c4bb706cdccdf175132ff939f78
Fenovka <fenovka1996@gmail.com>
2016-11-24 22:04:54 +0200

    add property name,surname,gender,birthday and getter setter for him

---
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/Gender.java
@@ -0,0 +1,8 @@
+package com.tneu.fcit.pzs.guessword.model;
+
+/**
+ * Created by Volodia on 11/24/2016.
+ */
+public enum Gender {
+    Male,Female
+}
--- app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
@@ -1,6 +1,7 @@
 package com.tneu.fcit.pzs.guessword.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by yp on 02.11.16.
@@ -9,6 +10,43 @@ public class User implements Serializable {
     String nick;
     String password;
     int score;
+
+    private String name;
+    private String surname;
+    private Gender gender;
+    private Date birtday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Enum getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirtday() {
+        return birtday;
+    }
+
+    public void setBirtday(Date birtday) {
+        this.birtday = birtday;
+    }
 
     public User(String nick, String password) {
         this.nick = nick;
