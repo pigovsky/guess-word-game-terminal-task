f455918248a10e477655d1b3eb70491d6c64c1f5
KoliaYakibchuk <cxcv999@gmail.com>
2016-11-29 15:09:57 +0200

    New fields

--- app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
@@ -1,6 +1,7 @@
 package com.tneu.fcit.pzs.guessword.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by yp on 02.11.16.
@@ -9,6 +10,43 @@ public class User implements Serializable {
     String nick;
     String password;
     int score;
+
+    String name;
+    String surname;
+    String sex;
+    Date data;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 
     public User(String nick, String password) {
         this.nick = nick;
