707eeef2e62f509fd0b5c7490be31f99b73442e9
Yevgen <gigabaytik13@gmail.com>
2016-11-30 00:45:38 +0200

    task #7 done

--- app/src/main/java/com/tneu/fcit/pzs/guessword/ui/WelcomeScreen.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/ui/WelcomeScreen.java
@@ -6,6 +6,8 @@ import com.tneu.fcit.pzs.guessword.service.UserServiceImpl;
 import com.tneu.fcit.pzs.guessword.utils.Utils;
 import com.tneu.fcit.pzs.guessword.view.GameViewImpl;
 
+import java.util.*;
+
 /**
  * Created by yp on 02.11.16.
  */
@@ -14,12 +16,32 @@ public class WelcomeScreen {
     private final UserService userService = new UserServiceImpl();
 
     public void showWelcome() {
-        System.out.println("Welcome! Please [l]ogin or [r]egister");
+        System.out.println("Welcome! Please [l]ogin, [r]egister or [s]how best results");
         String line = Utils.SCANNER.nextLine();
         if (line.equalsIgnoreCase("l")) {
             onLogin();
         } else if (line.equalsIgnoreCase("r")) {
             onRegister();
+        } else if (line.equalsIgnoreCase("s")) {
+            showRecord();
+
+        }
+
+    }
+
+    private void showRecord() {
+        System.out.println("Records table of users");
+
+        ArrayList list = (ArrayList) userService.all();
+
+        Collections.sort(list, (o1, o2) -> {
+            int a1 = ((User)o1).getScore();
+            int a2 = ((User)o1).getScore();
+            return a1 < a2 ? 1 : (a1 > a2 ? -1 : 0);
+        });
+
+        for (Object user : list) {
+            System.out.println(((User)user).getNick() + "\t" + ((User)user).getScore());
         }
     }
 
