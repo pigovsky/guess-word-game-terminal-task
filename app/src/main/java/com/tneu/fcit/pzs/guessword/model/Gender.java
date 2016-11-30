67723f6f0532fbb60ed6bcb94dd6555a6cf84793
Vadim Zabchuk <flikmrx@gmail.com>
2016-11-30 18:29:59 +0200

    add properties for user

---
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/Gender.java
@@ -0,0 +1,8 @@
+package com.tneu.fcit.pzs.guessword.model;
+
+/**
+ * Created by VADIM on 30.11.2016.
+ */
+public enum Gender {
+    Male,Female
+}
--- app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
@@ -1,6 +1,7 @@
 package com.tneu.fcit.pzs.guessword.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by yp on 02.11.16.
@@ -13,6 +14,43 @@ public class User implements Serializable {
     public User(String nick, String password) {
         this.nick = nick;
         this.password = password;
+    }
+
+    private String name;
+    private String surname;
+    private Gender gender;
+    private Date birtday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Enum getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirtday() {
+        return birtday;
+    }
+
+    public void setBirtday(Date birtday) {
+        this.birtday = birtday;
     }
 
     public String getNick() {
