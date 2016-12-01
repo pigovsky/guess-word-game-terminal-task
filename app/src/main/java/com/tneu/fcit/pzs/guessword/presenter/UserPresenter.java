6108437c583327cec4b95c0a194ce7f3a03d7420
Anatoliy <anatoliy.vasylyk@eleks.com>
2016-12-01 22:03:54 +0200

    changed to extract methods to user presenter

--- app/src/main/java/com/tneu/fcit/pzs/guessword/presenter/UserPresenter.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/presenter/UserPresenter.java
@@ -15,25 +15,41 @@ public class UserPresenter {
     public void updateProfile(User user) {
         user.printShortInfo();
 
+        updateName(user);
+        updateSurname(user);
+        updateBirthYear(user);
+        updateGender(user);
+        saveUser(user);
+
+        user.printShortInfo();
+    }
+
+    private void updateName(User user) {
         System.out.println("Enter new name: ");
         String nameInput = Utils.SCANNER.nextLine();
         user.setName(nameInput);
+    }
 
+    private void updateSurname(User user) {
         System.out.println("Enter new surname: ");
         String surnameInput = Utils.SCANNER.nextLine();
         user.setSurname(surnameInput);
+    }
 
+    private void updateBirthYear(User user) {
         System.out.println("Enter new birth year: ");
         String birthYearInput = Utils.SCANNER.nextLine();
         user.setBirthYear(Integer.parseInt(birthYearInput));
+    }
 
+    private void updateGender(User user) {
         System.out.println("Enter new gender (male, female, other): ");
         Gender genderInput = Gender.parse(Utils.SCANNER.nextLine());
         user.setGender(genderInput);
+    }
 
+    private void saveUser(User user) {
         userService.save(user);
-
         System.out.println("Update Profile SUCCESS!");
-        user.printShortInfo();
     }
 }
