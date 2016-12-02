4c6ff0a783e084c4394dccc11b164b2a5f851dac
chapelskyi <slavaternopil@ukr.net>
2016-12-02 11:21:11 +0200

    Added additional fields to user

--- app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
+++ app/src/main/java/com/tneu/fcit/pzs/guessword/model/User.java
@@ -8,11 +8,72 @@ import java.io.Serializable;
 public class User implements Serializable {
     String nick;
     String password;
+    String firstName;
+    String lastName;
+    String sex;
+    Date birth;
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     int score;
 
     public User(String nick, String password) {
         this.nick = nick;
         this.password = password;
+    }
+
+    public User(int score, Date birth, String sex, String lastName, String firstName, String password, String nick) {
+        this.score = score;
+        this.birth = birth;
+        this.sex = sex;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.password = password;
+        this.nick = nick;
+    }
+
+    public User(int score) {
+        this.score = score;
+    }
+
+    public User(String nick, String password, String firstName, String lastName, String sex, Date birth) {
+
+        this.nick = nick;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.birth = birth;
     }
 
     public String getNick() {
